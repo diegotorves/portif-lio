0673767779875ce4ad3ff635155267e2ca871772
diegotorves <39132740+diegotorves@users.noreply.github.com>
2026-03-28 18:47:06 -0300

    Atualizando Repositorio

---
+++ CleanArchMvc/CleanArchMVC.Domain/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMVC.Domain.Entities
+{
+    public sealed class Product
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; } = string.Empty;
+        public string Description { get; private set; } = string.Empty;
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+        public string Image { get; private set; } = string.Empty;
+
+        //Relacionamento
+        public int CategoryId { get; private set; }
+        public Category Category { get; private set; } 
+    }
+}
